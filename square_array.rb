ace9a047c6ece0be09182ba88eb15c5b12b5ed42
Mark Rego <rego121@gmail.com>
2019-08-14 18:51:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
-def square_array(array)
-  # your code here
+
+
+array [2,4,6,8]
+newarr = []
+i = 0
+ def square_array(array)
+
+while i <= array.length
+
+  array[i]*2
+  newarr << i
+end
+
+i += 1
+end
+return newarr
+
 end
